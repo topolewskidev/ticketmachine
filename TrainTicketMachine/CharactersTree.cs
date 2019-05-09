425ff92204b1f7a5742ee2a9385b41b984399634
mtopolewski <marcin.topolewski@skyrise.tech>
2019-05-09 08:54:01 +0200

    Added TreeNode class. Implemented insertion for CharactersTree

--- TrainTicketMachine/CharactersTree.cs
+++ TrainTicketMachine/CharactersTree.cs
@@ -2,9 +2,41 @@
 {
     public class CharactersTree : ISearchableTree
     {
+        public TreeNode Root { get; } = TreeNode.Empty;
+
         public void Insert(string item)
         {
-            throw new System.NotImplementedException();
+            var currentNode = GetParent(item);
+
+            for (int i = currentNode.Depth; i < item.Length; i++)
+            {
+                var node = new TreeNode(item[i], i + 1);
+
+                currentNode.AddChild(node);
+                currentNode = node;
+            }
+
+            currentNode.AddChild(new TreeNode(default(char), currentNode.Depth + 1));
+        }
+
+        public TreeNode GetParent(string text)
+        {
+            var currentNode = Root;
+            var result = currentNode;
+
+            foreach (var character in text)
+            {
+                currentNode = currentNode.TryGetChildNode(character);
+
+                if (currentNode == TreeNode.Empty)
+                {
+                    break;
+                }
+
+                result = currentNode;
+            }
+
+            return result;
         }
     }
 }
---
+++ TrainTicketMachine/TreeNode.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TrainTicketMachine
+{
+    public class TreeNode
+    {
+        public static readonly TreeNode Empty = new TreeNode(default(char), 0);
+
+        private readonly IList<TreeNode> _children = new List<TreeNode>();
+
+        public TreeNode(char value, int depth)
+        {
+            Value = value;
+            Depth = depth;
+        }
+
+        public int Depth { get; }
+
+        public char Value { get; }
+
+        public void AddChild(TreeNode childNode)
+        {
+            _children.Add(childNode);
+        }
+
+        public TreeNode TryGetChildNode(char searchValue)
+        {
+            var childNode = _children.SingleOrDefault(node => node.Value.Equals(searchValue));
+
+            if (childNode is null)
+            {
+                childNode = TreeNode.Empty;
+            }
+
+            return childNode;
+        }
+    }
+}
